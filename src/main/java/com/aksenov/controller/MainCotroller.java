0bbe53e9b81b5594261e31c61e4eb9ead2f08866
Aleksandr Aksenov <sasha.aksenov.01@mail.ru>
2022-04-30 15:21:59 +0300

    Initial commit

---
+++ src/main/java/com/aksenov/controller/MainCotroller.java
@@ -0,0 +1,65 @@
+package com.aksenov.controller;
+
+import com.aksenov.domain.Message;
+import com.aksenov.domain.User;
+import com.aksenov.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MainCotroller {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
+
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (!filter.isEmpty() && filter != null)
+            messages = messageRepository.findByTag(filter);
+        else
+            messages = messageRepository.findAll();
+
+        model.put("messages" ,messages);
+
+        return "main";
+    }
+}

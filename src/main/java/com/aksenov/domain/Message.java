29bfa9074829df568bcb6d890921f8ab91fef827
Aleksandr Aksenov <sasha.aksenov.01@mail.ru>
2022-05-20 17:22:58 +0300

    Add data validation. Add password encoding

--- src/main/java/com/aksenov/domain/Message.java
+++ src/main/java/com/aksenov/domain/Message.java
@@ -1,13 +1,21 @@
 package com.aksenov.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
+
+    @NotBlank(message = "Please enter the message")
+    @Length(max = 2048, message = "Message too long (more than 2048 symbols)")
     private String text;
+
+    @Length(max = 255, message = "Tag too long (more than 255 symbols)")
     private String tag;
 
     private String filename;

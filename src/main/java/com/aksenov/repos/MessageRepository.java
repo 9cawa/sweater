4a10623ef63f122a39b10b86567ac8ca2a50e1f9
Aleksandr Aksenov <sasha.aksenov.01@mail.ru>
2022-05-24 19:12:42 +0300

    Add message list and user's messages editor

--- src/main/java/com/aksenov/repos/MessageRepository.java
+++ src/main/java/com/aksenov/repos/MessageRepository.java
@@ -1,11 +1,11 @@
 package com.aksenov.repos;
 
 import com.aksenov.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface MessageRepository extends CrudRepository<Message, Long> {
     List<Message> findByTag(String tag);
 
     List<Message> findAllByOrderByIdDesc();
